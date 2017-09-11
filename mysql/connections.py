9c798cd97471f88612eae1b5fe32f63d3b2c1dca
besenthil <besenthil@gmail.com>
2017-09-11 11:46:01 +0530

    Read from a CSV File

--- mysql/connections.py
+++ mysql/connections.py
@@ -2,6 +2,12 @@ from abc import ABCMeta,abstractmethod
 
 from pyspark import SQLContext
 from pyspark import SparkContext
+from pyspark.sql.types import StructField
+from pyspark.sql.types import StructType
+from pyspark.sql.types import StringType
+from pyspark.sql.types import IntegerType
+from pyspark.sql.types import DoubleType
+from pyspark.sql import Row
 
 class Connection(object):
     __metaclass__ = ABCMeta
@@ -9,6 +15,79 @@ class Connection(object):
     @abstractmethod
     def __init__(self):
         pass
+
+class FileConnection(Connection):
+    def __init__(self,spark_context,file_path):
+        self.file_path=file_path
+        self.spark_context=spark_context
+        self.rdd=None
+        self.delimiter='\t'
+        self.sql_context = SQLContext(self.spark_context)
+        self.header=None
+
+    def read_file_to_rdd(self):
+        # self.rdd = self.spark_context.textFile(self.file_path)
+        pass
+
+
+
+    def rdd_to_df(self):
+        def generate_tuple(line):
+            return tuple(line)
+
+        def get_delimiter():
+            #return self.delimiter
+            return '\t'
+
+        # Split lines to structured lines
+        self.rdd = self.spark_context.textFile(self.file_path)
+        #delimit=self.delimit
+        self.delimit='\t'
+        header_record = self.rdd.first()
+        print (header_record)
+        rows = self.rdd.filter(lambda line: line != header_record)
+        delimited_rows = rows.map(lambda line: line.split(get_delimiter()))
+
+        # Convert structured lines to tuple
+        #print rows.count()
+        tuples = delimited_rows.map(generate_tuple)
+
+
+        ## Associate tuples to schema
+        #  Generate schema
+        if self.header is True:
+            schema = self.generate_schema(' '.join(self.header_string.split(self.delimiter)))
+        return self.sql_context.createDataFrame(
+                                data=tuples,
+                                schema=schema,
+                                verifySchema=True)
+
+    def generate_schema(self,schema_string=None):
+        if schema_string is None:
+            return Exception("Schema required")
+
+        fields = [StructField(field_name, StringType(), True)
+                  for field_name in schema_string.split()]
+        schema = StructType(fields)
+        return schema
+
+
+    def read_into_df(self,delimiter=None,header=False,header_string=None):
+        if delimiter is None:
+            raise Exception ("Delimiter required to read file")
+        self.delimiter = delimiter
+        self.header=header
+        self.header_string=header_string
+
+        self.read_file_to_rdd()
+        self.data_frame = self.rdd_to_df()
+        return self.data_frame
+
+
+
+
+
+
 
 class DatabaseConnection(Connection):
 
--- mysql/driver.py
+++ mysql/driver.py
@@ -1,11 +1,19 @@
 from connections import SparkMySQLConnector
 from pyspark import SparkContext as sc
+from connections import FileConnection
+HEADER='R G B Count'
 
 if __name__ == "__main__":
     spark_context = sc('local','example')
+    '''
     connection = SparkMySQLConnector(spark_context,
                                      "104.196.43.177",
                                      "spark",
                                      "spark",
                                      "spark",
                                      "senthil")
+    '''
+    file_path = 'file:///home/senthil/code/pyspark/fraud/data/skin.txt'
+    connection = FileConnection(spark_context,file_path)
+    data_frame=connection.read_into_df(delimiter='\t',header=True,header_string=HEADER)
+    print (data_frame.count())
